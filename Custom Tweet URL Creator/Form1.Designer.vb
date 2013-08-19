26428fb96e800ac44da1ace53d2ddf0e745811ba
EpicWolverine <epicwolverine@me.com>
2013-08-18 20:59:18 -0400

    Initial commit

---
+++ Custom Tweet URL Creator/Form1.Designer.vb
@@ -0,0 +1,142 @@
+﻿'    Copyright 2010, 2013 Brendan Ferracciolo
+'
+'    This file is part of Custom Tweet URL Creator.
+'
+'    Custom Tweet URL Creator is free software: you can redistribute it and/or modify
+'    it under the terms of the GNU General Public License as published by
+'    the Free Software Foundation, either version 3 of the License, or
+'    (at your option) any later version.
+'
+'    Custom Tweet URL Creator is distributed in the hope that it will be useful,
+'    but WITHOUT ANY WARRANTY; without even the implied warranty of
+'    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'    GNU General Public License for more details.
+'
+'    You should have received a copy of the GNU General Public License
+'    along with Custom Tweet URL Creator.  If not, see <http://www.gnu.org/licenses/>.
+
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(50, 81)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ReadOnly = True
+        Me.TextBox2.Size = New System.Drawing.Size(370, 20)
+        Me.TextBox2.TabIndex = 2
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.Transparent
+        Me.Button5.FlatAppearance.BorderSize = 0
+        Me.Button5.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent
+        Me.Button5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent
+        Me.Button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button5.Font = New System.Drawing.Font("Arial Black", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.ForeColor = System.Drawing.Color.Silver
+        Me.Button5.Location = New System.Drawing.Point(415, 13)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(51, 62)
+        Me.Button5.TabIndex = 3
+        Me.Button5.Text = "140"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.Enabled = False
+        Me.Button1.Location = New System.Drawing.Point(426, 81)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(40, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Copy"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(397, 63)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 107)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(441, 26)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Note: It is recommended that you test the link for accuracy before giving it out " & _
+    "to others. It is " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "also recommended that you use a link shortener (like bit.ly) " & _
+    "to make sharing your link easier."
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 84)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(32, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "URL:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(471, 137)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Custom Tweet URL Creator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+
+End Class
---
+++ Custom Tweet URL Creator/Form1.vb
@@ -0,0 +1,103 @@
+﻿'    Copyright 2010, 2013 Brendan Ferracciolo
+'
+'    This file is part of Custom Tweet URL Creator.
+'
+'    Custom Tweet URL Creator is free software: you can redistribute it and/or modify
+'    it under the terms of the GNU General Public License as published by
+'    the Free Software Foundation, either version 3 of the License, or
+'    (at your option) any later version.
+'
+'    Custom Tweet URL Creator is distributed in the hope that it will be useful,
+'    but WITHOUT ANY WARRANTY; without even the implied warranty of
+'    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'    GNU General Public License for more details.
+'
+'    You should have received a copy of the GNU General Public License
+'    along with Custom Tweet URL Creator.  If not, see <http://www.gnu.org/licenses/>.
+
+Imports System.Text
+
+Public Class Form1
+
+    Dim TweetLength As Short
+    Dim URL As String
+    Dim Tweet4URL As String
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox1.TextChanged
+        TweetLength = 140 - TextBox1.Text.Length
+        Button5.Text = TweetLength.ToString
+
+        If TweetLength <= 10 Then
+            Button5.ForeColor = Color.Red
+        ElseIf TweetLength <= 20 Then
+            Button5.ForeColor = Color.Maroon
+        ElseIf TweetLength > 20 Then
+            Button5.ForeColor = Color.Silver
+        End If
+
+        If TweetLength < 0 Then
+            TextBox2.Text = "Your tweet is too long!"
+            Button1.Enabled = False
+            Exit Sub
+        End If
+
+        TextBox2.Text = "Generating..."
+
+        Tweet4URL = TextBox1.Text
+
+        Dim s As System.Text.StringBuilder
+        s = New System.Text.StringBuilder(Tweet4URL)
+        s.Replace("%", "%25")
+        s.Replace("+", "%2B")
+        s.Replace(vbCrLf, "+")
+        s.Replace(" ", "+")
+        s.Replace("$", "%24")
+        s.Replace("&", "%26")
+        s.Replace(",", "%2C")
+        s.Replace("/", "%2F")
+        s.Replace(":", "%3A")
+        s.Replace(";", "%3B")
+        s.Replace("=", "%3D")
+        s.Replace("?", "%3F")
+        s.Replace("@", "%40")
+        s.Replace("""", "%22")
+        s.Replace("<", "%3C")
+        s.Replace(">", "%3E")
+        s.Replace("#", "%23")
+        s.Replace("{", "%7B")
+        s.Replace("}", "%7D")
+        s.Replace("|", "%7C")
+        s.Replace("\", "%5C")
+        s.Replace("^", "%5E")
+        s.Replace("~", "%7E")
+        s.Replace("[", "%5B")
+        s.Replace("]", "%5D")
+        s.Replace("`", "%60")
+        s.Replace("'", "%27")
+        Tweet4URL = s.ToString
+
+        TextBox2.Text = "http://twitter.com/home/?status=" & Tweet4URL
+
+        Button1.Enabled = True
+
+        If TextBox1.Text = "" Then
+            TextBox2.Text = ""
+            Button1.Enabled = False
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If TextBox2.Text = "" Then
+            Button1.Enabled = False
+        Else
+            Button1.Enabled = True
+            My.Computer.Clipboard.Clear()
+            My.Computer.Clipboard.SetText(TextBox2.Text)
+        End If
+    End Sub
+End Class
